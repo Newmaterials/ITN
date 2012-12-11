a7a7f960c4a03739cc6fe1675329da3be19ae3b8
Justin <justingier@gmail.com>
2012-12-10 20:50:33 -0800

    Fixing the Revert

--- js/script.js
+++ js/script.js
@@ -110,7 +110,8 @@ var monthHead = [],
 						'San Francisco',
 						'San Jose',
 						'Santa Clara',
-						'Seattle'
+						'Seattle',
+						'Zapopan'
 					];
 
 				for(var i=0; i<citiesToDisplayLeft.length; i++) {
@@ -205,22 +206,22 @@ var monthHead = [],
 			// var offsetSize = Math.floor( (numTouchPoints / root.maximumTotalPoints) * root.numBreakPoints) * imageWidth;
 
 			var offsetSize = 0;
-			if(numTouchPoints >= 800000) {
+			if(numTouchPoints >= 100000) {
 				offsetSize = 6;
 			} 
-			else if(numTouchPoints >= 400000 && numTouchPoints < 600000) {
+			else if(numTouchPoints >= 80000 && numTouchPoints < 100000) {
 				offsetSize = 5;
 			} 
-			else if(numTouchPoints >= 200000 && numTouchPoints < 400000) {
+			else if(numTouchPoints >= 60000 && numTouchPoints < 80000) {
 				offsetSize = 4;
 			} 
-			else if(numTouchPoints >= 100000 && numTouchPoints < 200000) {
+			else if(numTouchPoints >= 20000 && numTouchPoints < 60000) {
 				offsetSize = 3;
 			} 
-			else if(numTouchPoints >= 50000 && numTouchPoints < 100000) {
+			else if(numTouchPoints >= 10000 && numTouchPoints < 20000) {
 				offsetSize = 2;
 			} 
-			else if(numTouchPoints >= 20000 && numTouchPoints < 50000) {
+			else if(numTouchPoints >= 500 && numTouchPoints < 10000) {
 				offsetSize = 1;
 			} 
 			else {
@@ -349,26 +350,26 @@ var monthHead = [],
 			// };
 
 
-			if(root.eventData.TotalTouchPoints >= 800000) {
+			if(root.eventData.TotalTouchPoints >= 100000) {
 				fontSize = 28;
 			} 
-			else if(root.eventData.TotalTouchPoints >= 400000 && root.eventData.TotalTouchPoints < 600000) {
+			else if(root.eventData.TotalTouchPoints >= 80000 && root.eventData.TotalTouchPoints < 100000) {
 				fontSize = 24;
 			} 
-			else if(root.eventData.TotalTouchPoints >= 200000 && root.eventData.TotalTouchPoints < 400000) {
+			else if(root.eventData.TotalTouchPoints >= 60000 && root.eventData.TotalTouchPoints < 80000) {
+				fontSize = 21;
+			} 
+			else if(root.eventData.TotalTouchPoints >= 20000 && root.eventData.TotalTouchPoints < 60000) {
 				fontSize = 18;
 			} 
-			else if(root.eventData.TotalTouchPoints >= 100000 && root.eventData.TotalTouchPoints < 200000) {
+			else if(root.eventData.TotalTouchPoints >= 10000 && root.eventData.TotalTouchPoints < 20000) {
 				fontSize = 16;
 			} 
-			else if(root.eventData.TotalTouchPoints >= 50000 && root.eventData.TotalTouchPoints < 100000) {
-				fontSize = 14;
-			} 
-			else if(root.eventData.TotalTouchPoints >= 20000 && root.eventData.TotalTouchPoints < 50000) {
-				fontSize = 14;
+			else if(root.eventData.TotalTouchPoints >= 500 && root.eventData.TotalTouchPoints < 10000) {
+				fontSize = 16;
 			} 
 			else {
-				fontSize = 11;	
+				fontSize = 13;	
 			}
 
 			root.drawMarkerText(fontSize);
